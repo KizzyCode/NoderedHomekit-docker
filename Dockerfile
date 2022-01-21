e38aa78091ec6b1886a459060a146cfa09fe436e
Keziah Biermann <development@kizzycode.de>
2022-01-22 00:16:33 +0100

    Disable obsolete avahi integration

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,9 @@
 FROM nodered/node-red:latest
 USER root
 
-RUN apk add --no-cache dbus avahi-compat-libdns_sd avahi-dev ffmpeg \
-    && rm /etc/avahi/services/*.service
+RUN apk add --no-cache ffmpeg
 RUN npm config set cache /usr/src/node-red/.npm --global \
     && npm install --unsafe-perm --no-update-notifier --only=production node-red-contrib-homekit-bridged
-
-COPY ./files/avahi-daemon.conf /etc/avahi/avahi-daemon.conf
-COPY ./files/http.service /etc/avahi/services/http.service
 COPY ./nodered-homekit.sh /sbin/nodered-homekit
 
 ENTRYPOINT []
--- nodered-homekit.sh
+++ nodered-homekit.sh
@@ -1,35 +1,12 @@
 #!/bin/bash
 set -eu
 
-# Set hostname
-if test -z "$HOSTNAME"; then
-  echo "!> Missing \$HOSTNAME"
-  exit 1
-fi
-sed -i "s/.*host-name.*/host-name=$HOSTNAME/g" "/etc/avahi/avahi-daemon.conf"
-
-
-# Set port
-if test -z "$PORT"; then
-  echo "!> Missing \$PORT"
-  exit 1
-fi
-sed -i "s/.*<port>.*<\/port>.*/   <port>$PORT<\/port>/g" "/etc/avahi/services/http.service"
-
-
-# Spawn DBus
-dbus-daemon --system --nofork &
-disown
-until test -e "/var/run/dbus/system_bus_socket"; do
-  sleep 1s
-done
-
-
-# Spawn avahi daemon
-avahi-daemon --no-chroot -f /etc/avahi/avahi-daemon.conf &
-disown
-
+# Repair permissions
+chown -R node-red:node-red "/data"
+chmod u=rwX,g=,o= "/data"
 
 # Start NodeRED
-cd /usr/src/node-red
-node-red npm start -- --userDir /data
+su -l node-red <<EOF
+    cd /usr/src/node-red
+    npm --no-update-notifier --no-fund start --cache /data/.npm -- --userDir /data
+EOF
