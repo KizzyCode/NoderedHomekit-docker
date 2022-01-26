687f2ba3ebe13717acb01cab407aa4c36f9ec28a
Keziah Biermann <development@kizzycode.de>
2022-01-26 23:52:23 +0100

    Use exec to spawn npm

--- nodered-homekit.sh
+++ nodered-homekit.sh
@@ -6,7 +6,7 @@ chown -R node-red:node-red "/data"
 chmod u=rwX,g=,o= "/data"
 
 # Start NodeRED
-su -l node-red <<EOF
+exec su -l node-red <<EOF
     cd /usr/src/node-red
-    npm --no-update-notifier --no-fund start --cache /data/.npm -- --userDir /data
+    exec npm --no-update-notifier --no-fund start --cache /data/.npm -- --userDir /data
 EOF
